fcf4375a7e58597e4f39fff2281c6313b53d1d99
g4titanx <g4titan1@gmail.com>
2024-05-20 15:12:36 +0100

    impl CLAP handle the commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,53 @@
-pub struct Task {
-    pub name: String,
-    pub task: Vec<String>,
+use std::collections::HashMap;
+
+pub struct TaskManager {
+    tasks: HashMap<String, Vec<String>>,
 }
 
-impl Task {
-    pub fn create_task(
-        mut args: impl Iterator<Item = String>,
-    ) -> Result<Task, &'static str> {
-        args.next();
-
-        let name = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get input"),
-        };
-
-        let task: Vec<String> = args.collect();
-
-        if task.is_empty() {
-            return Err("Didn't get any tasks");
+impl TaskManager {
+    pub fn new() -> TaskManager {
+        TaskManager {
+            tasks: HashMap::new(),
         }
+    }
 
-        Ok(Task {
-            name,
-            task
-        })
+    pub fn add_task(&mut self, name: String, details: Vec<String>) {
+        self.tasks.insert(name, details);
+    }
+
+    pub fn list_tasks(&self) -> &HashMap<String, Vec<String>> {
+        &self.tasks
+    }
+
+    pub fn get_task(&self, name: &str) -> Option<&Vec<String>> {
+        self.tasks.get(name)
     }
 }
 
-pub fn run(task: Task) {
-    match Task::create_task(task) {
-        Ok(task) => {
-            println!("Task name: {}", task.name);
-            println!("Tasks:");
-            for t in task.task {
-                println!("- {}", t);
-            }
-        }
-        Err(e) => eprintln!("Error: {}", e),
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_and_get_task() {
+        let mut manager = TaskManager::new();
+        manager.add_task("TestTask".to_string(), vec!["Detail1".to_string(), "Detail2".to_string()]);
+        assert_eq!(manager.get_task("TestTask"), Some(&vec!["Detail1".to_string(), "Detail2".to_string()]));
     }
-} 
+
+    #[test]
+    fn test_list_tasks() {
+        let mut manager = TaskManager::new();
+        manager.add_task("Task1".to_string(), vec!["Detail1".to_string()]);
+        manager.add_task("Task2".to_string(), vec!["Detail2".to_string()]);
+        let tasks = manager.list_tasks();
+        assert!(tasks.contains_key("Task1"));
+        assert!(tasks.contains_key("Task2"));
+    }
+
+    #[test]
+    fn test_get_nonexistent_task() {
+        let manager = TaskManager::new();
+        assert!(manager.get_task("Nonexistent").is_none());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,56 @@
-use std::env;
-use std::process;
-use task_scheduler_cli::Task;
+use clap::{App, Arg};
+use task_scheduler_cli::TaskManager;
 
 fn main() {
-    let task = Task::create_task(env::args()).unwrap_or_else(|err| { 
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let matches = App::new("Task Manager")
+        .version("1.0")
+        .author("g4titan g4titan1@gmail.com")
+        .about("Manages tasks")
 
-    if let E = run(task) {
-        
+        .subcommand(App::new("add")
+            .about("Adds a new task")
+            .arg(Arg::with_name("name")
+                .help("Name of the task")
+                .required(true))
+            .arg(Arg::with_name("task")
+                .help("Task details")
+                .required(true)
+                .multiple(true)))
+
+        .subcommand(App::new("list")
+            .about("Lists all tasks"))
+
+        .subcommand(App::new("get")
+            .about("Gets a specific task")
+            .arg(Arg::with_name("name")
+                .help("Name of the task to get")
+                .required(true)))
+        .get_matches();
+
+    let mut task_manager = TaskManager::new();
+
+    if let Some(matches) = matches.subcommand_matches("add") {
+        let name = matches.value_of("name").unwrap();
+
+        let task_details: Vec<String> = matches.values_of("task").unwrap()
+            .map(String::from)
+            .collect();
+
+        task_manager.add_task(name.to_string(), task_details);
+        println!("Task '{}' added successfully.", name);
+    } 
+    
+    else if let Some(_matches) = matches.subcommand_matches("list") {
+        for (name, details) in task_manager.list_tasks() {
+            println!("Task '{}': {:?}", name, details);
+        }
+    } 
+    
+    else if let Some(matches) = matches.subcommand_matches("get") {
+        let name = matches.value_of("name").unwrap();
+        match task_manager.get_task(name) {
+            Some(task) => println!("Task '{}': {:?}", name, task),
+            None => println!("No task found with name '{}'", name),
+        }
     }
 }
